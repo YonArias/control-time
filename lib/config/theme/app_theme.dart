680b3a2b7f0946454d946da99eb72bbafde100ab
YonArias <jhonquequearias@gmail.com>
2023-12-01 18:27:43 -0500

    Configuramos los temas del app

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'El color no puede ser menor que 0'),
+        assert(selectedColor < colorList.length, 'El colorlist no puede ser mas que ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ],
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
+}
